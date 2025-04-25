97ddcdb230d4f5a2c0b218c438d2883bb7660ca5
chandrashekhar3337 <shekharara926290@gmail.com>
2025-04-25 09:59:27 +0530

    coonect to mongoDB

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ dotenv.config({
     path: './env'
 })
 import connectDB from "./db/db.js";
+import { app } from "./app.js"
 
 
 connectDB()
+.then( () =>{
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running at ${process.env.PORT} `)
+    })
+})
+.catch((err) =>{
+ console.log("mongo db connection failed !!!", err)
+}
+)
 
 
 
